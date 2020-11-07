2a215153ad4436255467eaa1502320947e521056
mesnuuu <46952459+mesnuuu@users.noreply.github.com>
2020-11-07 20:23:45 +0500

    Signup - exceptions - alert

--- src/main/java/com/smart/config/MyConfig.java
+++ src/main/java/com/smart/config/MyConfig.java
@@ -1,5 +1,10 @@
 package com.smart.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MyConfig {
+	
+	
 
 }
--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.smart.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.User;
+import com.smart.helper.Message;
 
 
 @Controller
@@ -57,26 +60,43 @@ public class HomeController {
 	
 	public String registerUser(@ModelAttribute ("user") User user, 
 							   @RequestParam(value="agreement", defaultValue = "false") boolean agreement, 
-							   Model model) 
+							   Model model,
+							   HttpSession session) 
 	{
 		
-		if(!agreement) {
-			System.out.println("You have not agreed the terms and conditions.");
+		try {
+			
+			if(!agreement) {
+				System.out.println("You have not agreed the terms and conditions.");
+				throw new Exception("You have not agreed the terms and conditions.");
+			}
+			
+			user.setRole("ROLE_USER");
+			user.setEnabled(true);
+			user.setImageUrl("default.png"); 
+			
+			// decrypt password encoder object
+			
+			System.out.println("Agreement " +agreement);
+			System.out.println("USER" +user);
+			
+			User result = this.userRepository.save(user);
+			
+			model.addAttribute("user", new User() ); 
+			
+			session.setAttribute("message", new Message("Successfully Registered !!" , "alert-success") );
+			return "signup";
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			model.addAttribute("user", user);
+			session.setAttribute("message", new Message("Something went wrong !!" + e.getMessage(), "alert-danger") );
+			return "signup";
+			
 		}
 		
-		user.setRole("ROLE_USER");
-		user.setEnabled(true);
-		
-		// decrypt password encoder object
-		
-		System.out.println("Agreement " +agreement);
-		System.out.println("USER" +user);
-		
-		User result = this.userRepository.save(user);
-		
-		model.addAttribute("user", result);
-		
-		return "signup";
 	}
 	
 	/*
